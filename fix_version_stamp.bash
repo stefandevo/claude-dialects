5a376c36f7b72848a690a249cc9d72a0c0c6e20d
Stefan de Vogelaere <stefan.de.vogelaere@gmail.com>
2026-07-23 11:59:43 +0200

    chore: clean up accidental script files in working tree - #26
    Remove patch automation scripts that were accidentally committed.

--- fix_version_stamp.bash
+++
@@ -1,3 +0,0 @@
-sed -i '' 's/_ = atomicWriteFile(versionPath, \[\]byte(embeddedProxyVersion()+"\\n"), 0o600)/_ = atomicWriteFile(versionPath, \[\]byte(CurrentAppVersion()+"\\n"), 0o600)/' internal/app/proxy.go
-sed -i '' 's/_ = atomicWriteFile(versionPath, \[\]byte(embeddedProxyVersion()+"\\n"), 0o600)/_ = atomicWriteFile(versionPath, \[\]byte(CurrentAppVersion()+"\\n"), 0o600)/' internal/app/cursor.go
-sed -i '' 's/_ = atomicWriteFile(versionPath, \[\]byte(embeddedProxyVersion()+"\\n"), 0o600)/_ = atomicWriteFile(versionPath, \[\]byte(CurrentAppVersion()+"\\n"), 0o600)/' internal/app/copilot.go
